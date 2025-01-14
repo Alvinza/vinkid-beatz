001e8edd514568a79a976c95273cdc43895d6033
Alvin Zondi <167542762+Alvinza@users.noreply.github.com>
2025-01-14 10:25:26 +0200

    Update Register.jsx

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -29,7 +29,7 @@ const Register = () => {
 
   const handleSubmit = async (values, { setSubmitting, resetForm }) => {
     try {
-      await axios.post("http://localhost:5000/api/register", values);
+      await axios.post("https://vinkid-beatz-backend.onrender.com/api/register", values);
       navigate("/login");
       toast.success("Registration successful. Please log in.");
       resetForm();
