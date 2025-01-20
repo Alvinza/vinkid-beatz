8d66662cae4b1ed46d41b2ee1d7aaa66726e6344
Alvin Zondi <167542762+Alvinza@users.noreply.github.com>
2025-01-20 06:41:36 +0200

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
+// import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { HashRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     
-  <BrowserRouter>
+  <HashRouter>
     {" "}
     {/* Wrap App with BrowserRouter for routing */}
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
 
 reportWebVitals();
