0102d34a068d22e8f0783be9f83de9b78bbd0712
Alvin Zondi <167542762+Alvinza@users.noreply.github.com>
2025-08-27 18:23:20 +0200

    Update beatRoutes.js

--- backend/routes/beatRoutes.js
+++ backend/routes/beatRoutes.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const Beat = require("../models/Beat");
-const { verifyAdmin } = require("../middleware/authMiddleware");
+const { verifyToken, verifyAdmin } = require("../middleware/authMiddleware");
 
 // ========================
 // Get all beats
